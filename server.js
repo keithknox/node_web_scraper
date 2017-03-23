e6d5074a92d93c88c9b6212c83fb2acf570fe33f
keithknox <keithwnknox@gmail.com>
2017-03-23 11:23:40 -0400

    created server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var fs = require("fs");
+var request = require("request");
+var cheerio = require("cheerio");
+var app = express();
+
+app.get('/scrape', function(req, res){
+
+})
+
+app.listen('8081');
+console.log('Server is now up and running on port:8081.');
+exports = module.exports = app;
